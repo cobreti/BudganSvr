63a0f91202b32ca2c98171e503846a115e17ff23
Danny Thibaudeau <cobreti_@hotmail.com>
2026-07-26 10:54:22 -0400

    updated ErrorValue SmartEnum

--- BudganGlobal/Errors/ErrorValue.cs
+++ BudganGlobal/Errors/ErrorValue.cs
@@ -2,14 +2,23 @@ namespace BudganGlobal.Errors;
 
 public class ErrorValue
 {
+    private static readonly Dictionary<string, ErrorValue> ByCode = new();
+
     public static ErrorValue ResourceNotFound = new ErrorValue("ResourceNotFound", "Resource not found");
-    
+
     public string ErrorCode { get; }
     public string ErrorMessage { get; }
-    
+
     protected ErrorValue(string errorCode, string errorMessage)
     {
         this.ErrorCode = errorCode;
         this.ErrorMessage = errorMessage;
+
+        ByCode.Add(errorCode, this);
+    }
+
+    public static ErrorValue FromCode(string errorCode)
+    {
+        return ByCode[errorCode];
     }
 }
